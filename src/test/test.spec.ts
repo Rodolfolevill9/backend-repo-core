85f9b57be127d3a8f6f94f8fb58ecae154735251
Rodolfo Levill <rodolfo.levill@globant.com>
2023-07-11 17:54:12 -0400

    lint issues

--- src/test/test.spec.ts
+++ src/test/test.spec.ts
@@ -2,15 +2,15 @@ import HttpRequest from '../httpRequestPage';
 import BasePage from '../basePage';
 
 const _axios = new HttpRequest();
-const _basePage =new BasePage();
+const _basePage = new BasePage();
 
 
 const authParams = _basePage.getAuthParams();
 
 describe('Unit testing', () => {
-    test('Get method', async () => {
-
+   test('Get method', async () => {
         const resp = await _axios.axiosGet(`https://api.trello.com/1/boards/6286b3d99ce1f98edbf83ddb/lists?${authParams}`);
         expect(resp.status).toBe(200);
     });
+    
 });
