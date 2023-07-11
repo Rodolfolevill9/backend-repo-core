9aa88e56263650144517c32a255e93aca057793a
Rodolfo Levill <rodolfo.levill@globant.com>
2023-07-11 18:07:11 -0400

    c

--- src/test/test.spec.ts
+++ src/test/test.spec.ts
@@ -9,7 +9,8 @@ const authParams = _basePage.getAuthParams();
 describe('Unit testing', () => {
   test('Get method', async () => {
     const resp = await _axios.axiosGet(
-      `https://api.trello.com/1/boards/6286b3d99ce1f98edbf83ddb/lists?${authParams}`);
+      `https://api.trello.com/1/boards/6286b3d99ce1f98edbf83ddb/lists?${authParams}`
+    );
     expect(resp.status).toBe(200);
   });
 });
